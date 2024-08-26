922a085c6468b97e2334315e8aabadc695bb5983
Natasha Simone <68391671+NatSim@users.noreply.github.com>
2024-08-26 21:51:39 +0100

    update: add the variants to logos

--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -1,51 +1,101 @@
-import styles from './Hero.module.css'
+import styles from "./Hero.module.css";
 //logos
-import heroImg from '../../assets/hero-image.jpg'
-import webSunny from '../../assets/wb_sunny.png'
-import webMoon from '../../assets/dark_mode_moon.png'
-import XIcon from '@mui/icons-material/X';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import heroImg from "../../assets/hero-image.jpg";
+import webSunny from "../../assets/wb_sunny.png";
+import webMoon from "../../assets/dark_mode_moon.png";
+import XIcon from "@mui/icons-material/X";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 //react
-import { useTheme } from '../../common/ThemeContext'
-
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
-    const {theme,toggleTheme}=useTheme();
-    const themeIcon = theme === 'light'? webSunny : webMoon;
-    const twitterIcon = theme === 'light'? <XIcon/>:<XIcon/>;
-    const githubIcon = theme === 'light'? <GitHubIcon/>:<GitHubIcon/>;
-    const linkedinIcon = theme === 'light'? <LinkedInIcon/>:<LinkedInIcon/>;
-    return (
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? webSunny : webMoon;
+
+  //   Icons
+  const twitterIcon =
+    theme === "light" ? (
+      <XIcon
+        className={styles.twitter}
+        href='https://twitter.com/tasha_simone5'
+        alt=''
+        sx={{ ...{ filter: "invert(1)" } }}
+      ></XIcon>
+    ) : (
+      <XIcon
+        href='https://twitter.com/tasha_simone5'
+        alt=''
+        className={styles.twitter}
+      ></XIcon>
+    );
+  const githubIcon =
+    theme === "light" ? (
+      <GitHubIcon
+        className={styles.github}
+        href='https://github.com/NatSim'
+        alt=''
+        sx={{ ...{ filter: "invert(1)" } }}
+      ></GitHubIcon>
+    ) : (
+      <GitHubIcon
+        className={styles.github}
+        href='https://github.com/NatSim'
+        alt=''
+      ></GitHubIcon>
+    );
+  const linkedinIcon =
+    theme === "light" ? (
+      <LinkedInIcon
+        className={styles.linkedin}
+        alt={"linkedin icon"}
+        sx={{ ...{ filter: "invert(1)" } }}
+      ></LinkedInIcon>
+    ) : (
+      <LinkedInIcon
+        className={styles.linkedin}
+        alt={"linkedin icon"}
+      ></LinkedInIcon>
+    );
+  return (
     <>
-    {/* <X></X> */}
-    <section className={styles.container}> 
-    <div><img className={styles.hero} src={heroImg} alt='' />
-    <img className={styles.colorMode} src={themeIcon} alt={'mode icon'} onClick={toggleTheme}/>  
-
-
-    </div>
-    </section>
-    <div className={styles.info}><h1>Natasha <br />Simone</h1>
-    <h2>Frontend Developer</h2>
-    <span><XIcon href='https://twitter.com/tasha_simone5'  alt='' ></XIcon>
-    
-
-   <GitHubIcon className={styles.github} href='https://github.com/NatSim'  alt='' ></GitHubIcon>
-    
-   
-   <LinkedInIcon className={styles.linkedin} src={linkedinIcon} alt={'linkedin icon'}></LinkedInIcon>
-</span>
-<p>Creativity is intelligence having fun ~ anonymous</p>
-<a>
-    <button className='hover' download>CV</button>
-</a>
-
-</div>
-    
-   </>
-  )
+      <section className={styles.container}>
+        {/* Website Mode Dark/Light */}
+        <div>
+          <img className={styles.hero} src={heroImg} alt='' />
+          <img
+            className={styles.colorMode}
+            src={themeIcon}
+            alt={"mode icon"}
+            onClick={toggleTheme}
+          />
+        </div>
+      </section>
+      {/* Intro */}
+      <div className={styles.info}>
+        <h1>
+          Natasha <br />
+          Simone
+        </h1>
+        <h2>Frontend Developer</h2>
+        <span>
+          {/* Twitter */}
+          {twitterIcon}
+          {/* Github */}
+          {githubIcon}
+          {/* Linkedin */}
+          {linkedinIcon}
+        </span>
+        <p>Creativity is intelligence having fun ~ anonymous</p>
+        <a>
+          <button className='hover' download>
+            CV
+          </button>
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default Hero;
